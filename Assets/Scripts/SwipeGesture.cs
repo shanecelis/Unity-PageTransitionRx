e443f101270327ef3e6c287764decc63b13e38e6
Shane Celis <shane.celis@gmail.com>
2019-06-13 00:45:32 -0400

    style: Reformat the whitespace a little.

--- Assets/Scripts/SwipeGesture.cs
+++ Assets/Scripts/SwipeGesture.cs
@@ -36,51 +36,51 @@ public class SwipeGesture : MonoBehaviour
     private Vector2 beginPosition;
     private DateTime beginTime;
 
-    void OnEnable() 
-    { 
-        var  eventTrigger = this.gameObject.AddComponent < ObservableEventTrigger > (); 
+    void OnEnable()
+    {
+        var  eventTrigger = this.gameObject.AddComponent < ObservableEventTrigger > ();
 
-        eventTrigger 
-            . OnBeginDragAsObservable () 
-            . TakeUntilDisable ( this ) 
-            . Where ( eventData => eventData.pointerDrag.gameObject == this.gameObject ) 
-            . Select ( eventData => eventData . position ) 
-            . Subscribe ( position => 
-            { 
-                this . beginPosition =  position ; 
-                this . beginTime  =  DateTime.Now ; 
-            }); 
+        eventTrigger
+            . OnBeginDragAsObservable ()
+            . TakeUntilDisable ( this )
+            . Where ( eventData => eventData.pointerDrag.gameObject == this.gameObject )
+            . Select ( eventData => eventData . position )
+            . Subscribe ( position =>
+            {
+                this . beginPosition =  position ;
+                this . beginTime  =  DateTime.Now ;
+            });
 
-        var  onEndDragObservable  =  eventTrigger 
-            . OnEndDragAsObservable () 
-            . TakeUntilDisable ( this ) 
-            . Where ( eventData => (DateTime.Now - this.beginTime).TotalSeconds < this.ThresholdSenconds ) 
-            . Select ( eventData  => eventData.position ) 
-            . Share (); 
+        var  onEndDragObservable  =  eventTrigger
+            . OnEndDragAsObservable ()
+            . TakeUntilDisable ( this )
+            . Where ( eventData => (DateTime.Now - this.beginTime).TotalSeconds < this.ThresholdSenconds )
+            . Select ( eventData  => eventData.position )
+            . Share ();
 
-        // left 
-        onEndDragObservable 
-            . Where ( position  =>  beginPosition.x  >  position.x ) 
-            . Where ( position  =>  Mathf . Abs ( beginPosition . x  -  position.x ) >= this.ThresholdDistance ) 
-            . Subscribe ( _  =>  onSwipeLeft . OnNext ( Unit.Default )) ; 
+        // left
+        onEndDragObservable
+            . Where ( position  =>  beginPosition.x  >  position.x )
+            . Where ( position  =>  Mathf . Abs ( beginPosition . x  -  position.x ) >= this.ThresholdDistance )
+            . Subscribe ( _  =>  onSwipeLeft . OnNext ( Unit.Default )) ;
 
-        // right 
-        onEndDragObservable 
-            . Where ( position => position.x  >  beginPosition . x ) 
-            . Where ( position => Mathf . Abs ( position . x  -  beginPosition.x ) >= this.ThresholdDistance ) 
-            . Subscribe ( _  => onSwipeRight . OnNext ( Unit.Default )); 
+        // right
+        onEndDragObservable
+            . Where ( position => position.x  >  beginPosition . x )
+            . Where ( position => Mathf . Abs ( position . x  -  beginPosition.x ) >= this.ThresholdDistance )
+            . Subscribe ( _  => onSwipeRight . OnNext ( Unit.Default ));
 
-        // down 
-        onEndDragObservable 
-            . Where ( position => beginPosition.y > position.y ) 
-            . Where ( position => Mathf.Abs (beginPosition.y  -  position.y ) >= this.ThresholdDistance ) 
-            . Subscribe ( _ => onSwipeDown.OnNext (Unit.Default)); 
+        // down
+        onEndDragObservable
+            . Where ( position => beginPosition.y > position.y )
+            . Where ( position => Mathf.Abs (beginPosition.y  -  position.y ) >= this.ThresholdDistance )
+            . Subscribe ( _ => onSwipeDown.OnNext (Unit.Default));
 
-        // up 
-        onEndDragObservable 
-            . Where ( position => position.y > beginPosition.y ) 
-            . Where ( position => Mathf . Abs ( position.y - beginPosition.y ) >=  this.ThresholdDistance ) 
-            . Subscribe ( _ => onSwipeUp.OnNext (Unit.Default)); 
+        // up
+        onEndDragObservable
+            . Where ( position => position.y > beginPosition.y )
+            . Where ( position => Mathf . Abs ( position.y - beginPosition.y ) >=  this.ThresholdDistance )
+            . Subscribe ( _ => onSwipeUp.OnNext (Unit.Default));
 
     }
 }
