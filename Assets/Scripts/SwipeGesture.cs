f9eb2ba776e805ada389efb9da3a51f775f3e6f9
Shane Celis <shane.celis@gmail.com>
2019-06-14 22:46:02 -0400

    feature: Swipe with dynamical feedback in UI.

--- Assets/Scripts/SwipeGesture.cs
+++ Assets/Scripts/SwipeGesture.cs
@@ -2,6 +2,7 @@
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SwipeGesture : MonoBehaviour
 {
@@ -10,35 +11,59 @@ public class SwipeGesture : MonoBehaviour
   public float ThresholdDistance = 100.0f;
 
   private Subject<Unit> onSwipeLeft = new Subject<Unit>();
-  public IObservable<Unit> OnSwipeLeft
-  {
-    get { return onSwipeLeft; }
-  }
+  public IObservable<PointerEventData> OnSwipeLeft;
+  // {
+  //   get { return onSwipeLeft; }
+  // }
 
   private Subject<Unit> onSwipeRight = new Subject<Unit>();
-  public IObservable<Unit> OnSwipeRight
-  {
-    get { return onSwipeRight; }
-  }
+  public IObservable<PointerEventData> OnSwipeRight;
+  // {
+  //   get { return onSwipeRight; }
+  // }
+
+  // private Subject<PointerEventData> onSwipeRight2 = new Subject<PointerEventData>();
+  // public IObservable<PointerEventData> OnSwipeRight2
+  // {
+  //   get { return onSwipeRight2; }
+  // }
 
   private Subject<Unit> onSwipeDown = new Subject<Unit>();
-  public IObservable<Unit> OnSwipeDown
-  {
-    get { return onSwipeDown; }
-  }
+  public IObservable<PointerEventData> OnSwipeDown;
+  // {
+  //   get { return onSwipeDown; }
+  // }
 
   private Subject<Unit> onSwipeUp = new Subject<Unit>();
-  public IObservable<Unit> OnSwipeUp
+  public IObservable<PointerEventData> OnSwipeUp;
+  // {
+  //   get { return onSwipeUp; }
+  // }
+
+  private Subject<Vector2> onDragRelative = new Subject<Vector2>();
+  public IObservable<Vector2> OnDragRelative
   {
-    get { return onSwipeUp; }
+    get { return onDragRelative; }
   }
 
   private Vector2 beginPosition;
   private DateTime beginTime;
 
+  public IObservable<PointerEventData> onEndDragObservable;
+
+  public IObservable<PointerEventData> OnSwipeWhere(Func<MoveDirection, bool> predicate) {
+    return onEndDragObservable
+      .Where(eventData => predicate(SwipeDirection(eventData)));
+  }
+
   void OnEnable()
   {
-    var  eventTrigger = this.gameObject.AddComponent < ObservableEventTrigger > ();
+    Debug.Log("Enabled");
+    var eventTrigger = this.gameObject.AddComponent < ObservableEventTrigger > ();
+
+    // eventTrigger
+    //   .OnBeginDragAsObservable ()
+    //   .Subscribe(_ => Debug.Log("Begin Drag."));
 
     eventTrigger
      .OnBeginDragAsObservable ()
@@ -51,36 +76,96 @@ public class SwipeGesture : MonoBehaviour
             this.beginTime  =  DateTime.Now ;
           });
 
-    var  onEndDragObservable  =  eventTrigger
-     .OnEndDragAsObservable ()
-     .TakeUntilDisable(this)
-     .Where(eventData => (DateTime.Now - this.beginTime).TotalSeconds < this.ThresholdSenconds)
-     .Select(eventData  => eventData.position)
-     .Share ();
+    eventTrigger
+      .OnDragAsObservable ()
+      .TakeUntilDisable(this )
+      .Where(eventData => eventData.pointerDrag.gameObject == this.gameObject )
+      // .Select(eventData => eventData.position - eventData.pressPosition)
+      .Select(eventData => eventData.delta)
+      .Subscribe(delta => onDragRelative.OnNext(delta));
 
-    // left
-    onEndDragObservable
-     .Where(position  =>  beginPosition.x  >  position.x)
-     .Where(position  =>  Mathf.Abs(beginPosition.x  -  position.x)>= this.ThresholdDistance)
-     .Subscribe(_  =>  onSwipeLeft.OnNext(Unit.Default)) ;
 
-    // right
-    onEndDragObservable
-     .Where(position => position.x  >  beginPosition.x)
-     .Where(position => Mathf.Abs(position.x  -  beginPosition.x)>= this.ThresholdDistance)
-     .Subscribe(_  => onSwipeRight.OnNext(Unit.Default));
+    eventTrigger
+      .OnEndDragAsObservable ()
+      .TakeUntilDisable(this )
+      .Where(eventData => eventData.pointerDrag.gameObject == this.gameObject )
+      // .Select(eventData => eventData.position - eventData.pressPosition)
+      .Select(eventData => eventData.delta)
+      .Subscribe(delta => onDragRelative.OnNext(delta));
 
-    // down
-    onEndDragObservable
-     .Where(position => beginPosition.y > position.y)
-     .Where(position => Mathf.Abs (beginPosition.y  -  position.y)>= this.ThresholdDistance)
-     .Subscribe(_ => onSwipeDown.OnNext (Unit.Default));
+    onEndDragObservable = eventTrigger
+      .OnEndDragAsObservable()
+      .TakeUntilDisable(this)
+      .Where(eventData => (DateTime.Now - this.beginTime).TotalSeconds < this.ThresholdSenconds);
+      // .Select(eventData => eventData.position )
+      // .Share();
 
-    // up
-    onEndDragObservable
-     .Where(position => position.y > beginPosition.y)
-     .Where(position => Mathf.Abs(position.y - beginPosition.y)>=  this.ThresholdDistance)
-     .Subscribe(_ => onSwipeUp.OnNext (Unit.Default));
+    // var  onEndDragObservable2  =  eventTrigger
+    //   .OnEndDragAsObservable ()
+    //   .TakeUntilDisable(this )
+    //   .Where(eventData => (DateTime.Now - this.beginTime).TotalSeconds < this.ThresholdSenconds )
+    //   // .Select(eventData  => eventData.position )
+    //   .Share ();
 
+    OnSwipeLeft = onEndDragObservable
+      .Where(eventData => SwipeDirection(eventData) == MoveDirection.Left);
+
+    OnSwipeRight = onEndDragObservable
+      .Where(eventData => SwipeDirection(eventData) == MoveDirection.Right);
+
+    OnSwipeUp = onEndDragObservable
+      .Where(eventData => SwipeDirection(eventData) == MoveDirection.Up);
+
+    OnSwipeDown = onEndDragObservable
+      .Where(eventData => SwipeDirection(eventData) == MoveDirection.Down);
+
+    // // left
+    // onEndDragObservable
+    //  .Where(position  =>  beginPosition.x  >  position.x )
+    //  .Where(position  =>  Mathf.Abs(beginPosition.x  -  position.x ) >= this.ThresholdDistance )
+    //  .Subscribe(_  =>  onSwipeLeft.OnNext(Unit.Default )) ;
+
+    // // right
+    // // onEndDragObservable
+    // //  .Where(position => position.x  >  beginPosition.x )
+    // //  .Where(position => Mathf.Abs(position.x  -  beginPosition.x ) >= this.ThresholdDistance )
+    // //  .Subscribe(_  => onSwipeRight.OnNext(Unit.Default ));
+    // // right
+    // onEndDragObservable2
+    //  .Where(eventData => eventData.position.x  >  beginPosition.x )
+    //  .Where(eventData => Mathf.Abs(eventData.position.x  -  beginPosition.x ) >= this.ThresholdDistance )
+    //  .Subscribe(eventData => onSwipeRight2.OnNext(eventData));
+
+    // // down
+    // onEndDragObservable
+    //  .Where(position => beginPosition.y > position.y )
+    //  .Where(position => Mathf.Abs (beginPosition.y  -  position.y ) >= this.ThresholdDistance )
+    //  .Subscribe(_ => onSwipeDown.OnNext (Unit.Default));
+
+    // // up
+    // onEndDragObservable
+    //  .Where(position => position.y > beginPosition.y )
+    //  .Where(position => Mathf.Abs(position.y - beginPosition.y ) >=  this.ThresholdDistance )
+    //  .Subscribe(_ => onSwipeUp.OnNext (Unit.Default));
+  }
+
+  public MoveDirection SwipeDirection(PointerEventData eventData) {
+    var delta_x = eventData.position.x - beginPosition.x;
+    var delta_y = eventData.position.y - beginPosition.y;
+    if (Mathf.Max(Mathf.Abs(delta_x), Mathf.Abs(delta_y)) >= this.ThresholdDistance) {
+      // We have a swipe.
+      if (Mathf.Abs(delta_x) > Mathf.Abs(delta_y)) {
+        return delta_x > 0 ? MoveDirection.Right : MoveDirection.Left;
+      } else {
+        return delta_y > 0 ? MoveDirection.Up : MoveDirection.Down;
+      }
+    } else {
+      return MoveDirection.None;
+    }
   }
 }
+
+// public static class SwipeGestureExtensions {
+
+//   public static IObservable<PointerEventData> WhereSwipe(IObservable<PointerEventData
+// }
--- Assets/Scripts/TurnPage.cs
+++ Assets/Scripts/TurnPage.cs
@@ -2,53 +2,121 @@
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx.Triggers;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(SwipeGesture), typeof(HorizontalLayoutGroup))]
 public class TurnPage : MonoBehaviour
 {
-    public float PageWidth = 1080;
+  public float PageWidth = 1080;
 
-    private RectTransform rectTransform;
-    private SwipeGesture swipeGesture;
-    private Tween moveAnimation;
-    private int pageCount;
-    private int currentPage = 1;
+  private RectTransform rectTransform;
+  private SwipeGesture swipeGesture;
+  private Tween moveAnimation;
+  private int pageCount;
+  private int currentPage = 0;
+  private Vector2 anchorBeginPosition;
+  private Vector2 pageWidthVector;
 
-    void Awake()
-    {
-        DOTween.Init();
-        DOTween.defaultAutoPlay = AutoPlay.None;  // Tween 생성시 자동 실행시키지
-    }
+  void Awake()
+  {
+    DOTween.Init();
+    DOTween.defaultAutoPlay = AutoPlay.None;  // Tween 생성시 자동 실행시키지
 
-    void OnEnable()
-    {
-        this.rectTransform  =  this.GetComponent<RectTransform>();
-        this.swipeGesture  =  this.GetComponent<SwipeGesture>();
-        this.pageCount  =  this.transform.childCount ;
+    pageWidthVector = new Vector2(-PageWidth, 0f);
+  }
 
-        // next
-        this . swipeGesture
-            . OnSwipeLeft
-            . Where ( _  =>  currentPage  <  pageCount )  // 최대 페이지 이전 인 경우에만 진행
-            . Where ( _  =>  this.moveAnimation  ==  null  ||  ! this.moveAnimation.IsPlaying ())  // 애니메이션 실행 중이 아니
-            . Subscribe ( _ =>
-                {
-                    this.currentPage ++;
-                    this.moveAnimation  =  this.rectTransform.DOAnchorPosX (rectTransform.anchoredPosition.x - this.PageWidth, 1.0f )
-                    .SetEase ( Ease.OutBounce )
-                    .Play ();
-                });
+  void OnEnable()
+  {
+    // Debug.Log("Enabled");
 
-        // back
-        this . swipeGesture
-            . OnSwipeRight
-            . Where ( _  =>  currentPage  >  1 )  // 1 페이지 이상인 경우에만 돌아갈
-            . Where ( _  =>  this.moveAnimation == null || !this.moveAnimation.IsPlaying())  // 애니메이션 실행 중이 없다
-            . Subscribe ( _  =>
-                {
-                    this.currentPage--;
-                    this.moveAnimation = this.rectTransform.DOAnchorPosX(rectTransform.anchoredPosition.x + this.PageWidth, 1.0f)
-                        .SetEase(Ease.OutBounce).Play();
-                } );
-    }
+    // eventTrigger
+    //   .OnBeginDragAsObservable ()
+    //   .TakeUntilDisable(this)
+    //   .Where(eventData => eventData.pointerDrag.gameObject == this.gameObject)
+    //   .Subscribe(_ => {
+    //       anchorBeginPosition = rectTransform.anchoredPosition;
+    //     });
+
+    var eventTrigger = GetComponent<ObservableEventTrigger>();
+    this.rectTransform  =  this.GetComponent<RectTransform>();
+    anchorBeginPosition = rectTransform.anchoredPosition;
+    this.swipeGesture  =  this.GetComponent<SwipeGesture>();
+    this.pageCount  =  this.transform.childCount ;
+    this.swipeGesture.OnDragRelative
+      .Where(_  =>  this.moveAnimation  ==  null  ||  ! this.moveAnimation.IsPlaying ())  // 애니메이션 실행 중이 아니
+      .Subscribe(delta =>
+          {
+           // Debug.Log("delta " + delta);
+            rectTransform.anchoredPosition += delta;
+          });
+
+    // // next
+    // this.swipeGesture
+    //  .OnSwipeLeft
+    //  .Where(_  =>  currentPage  <  pageCount - 1)  // 최대 페이지 이전 인 경우에만 진행
+    //  .Where(_  =>  this.moveAnimation  ==  null  ||  ! this.moveAnimation.IsPlaying ())  // 애니메이션 실행 중이 아니
+    //  .Subscribe(_ =>
+    //       {
+    //         this.currentPage++;
+    //         this.moveAnimation  =  this.rectTransform
+    //         .DOAnchorPos (anchorBeginPosition +  currentPage * this.pageWidthVector, 1.0f)
+    //         .SetEase(Ease.OutBounce)
+    //         .Play ();
+    //       });
+
+    // // back
+    // this.swipeGesture
+    //  .OnSwipeRight
+    //  .Where(_  =>  currentPage  >  0)  // 1 페이지 이상인 경우에만 돌아갈
+    //  .Where(_  =>  this.moveAnimation == null || !this.moveAnimation.IsPlaying())  // 애니메이션 실행 중이 없다
+    //  .Subscribe(_  =>
+    //       {
+    //         this.currentPage--;
+    //         this.moveAnimation = this.rectTransform
+    //         .DOAnchorPos(anchorBeginPosition + currentPage * this.pageWidthVector, 1.0f)
+    //         .SetEase(Ease.OutBounce).Play();
+    //       });
+
+    swipeGesture
+      .OnSwipeWhere(moveDirection =>
+          {
+            if (this.moveAnimation != null && this.moveAnimation.IsPlaying())
+              return false;
+
+            switch (moveDirection) {
+              case MoveDirection.Right:
+              if (currentPage > 0)
+                this.currentPage--;
+              break;
+              case MoveDirection.Left:
+              if (currentPage < pageCount - 1)
+                this.currentPage++;
+              break;
+            }
+            return true;
+          })
+      .Subscribe(_ =>
+          {
+            this.moveAnimation = this.rectTransform
+            .DOAnchorPos(anchorBeginPosition + currentPage * this.pageWidthVector, 1.0f)
+            .SetEase(Ease.OutBounce).Play();
+          });
+
+    // back
+    // this.swipeGesture
+    //  .OnSwipeRight2
+    //  .Where(_  =>  currentPage  >  1)  // 1 페이지 이상인 경우에만 돌아갈
+    //  .Where(_  =>  this.moveAnimation == null || !this.moveAnimation.IsPlaying())  // 애니메이션 실행 중이 없다
+    //  .Subscribe(eventData  =>
+    //       {
+    //         this.currentPage--;
+    //         this.moveAnimation = this.rectTransform
+    //         .DOAnchorPos(rectTransform.anchoredPosition
+    //                       - (eventData.position - eventData.pressPosition)
+    //                       + new Vector2(this.PageWidth, 0f),
+    //                       1.0f)
+    //         .SetEase(Ease.OutBounce).Play();
+    //       });
+  }
 }
